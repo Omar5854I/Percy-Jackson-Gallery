ae55e8f4e640277b9957b0afd7b3d43e366a5d54
Omar5854I <meromaro2010hamodi@gmail.com>
2026-07-22 00:45:50 +0300

    Changes

--- main.js
+++ main.js
@@ -10,21 +10,28 @@ renderer.outputColorSpace = THREE.SRGBColorSpace
 
 const loader = new THREE.TextureLoader()
 const texture1 = loader.load("./assets/PJO.jpg")
+const texture2 = loader.load("./assets/AJO.jpg")
+
 texture1.colorSpace = THREE.SRGBColorSpace
+texture2.colorSpace = THREE.SRGBColorSpace
 
 
 const geometry = new THREE.BoxGeometry(0.6,0.6,0.6)
 const material = new THREE.MeshBasicMaterial({map:texture1})
+const material2 = new THREE.MeshBasicMaterial({map:texture2})
 
 const cube = new THREE.Mesh(geometry, material)
+const cube2 = new THREE.Mesh(geometry, material2)
 
 const bgTexture = loader.load("./assets/PJOB.jpg")
 
 scene.background = bgTexture
 
 cube.position.set(1, 0, 0)
+cube2.position.set(0, -1, 0)
 
 camera.position.z = 2
+camera.position.x = -1
 
 
 let model
@@ -43,7 +50,7 @@ gltfLoader.load("./assets/PJS.glb", function(gltf){
     model = gltf.scene
     model.scale.set(0.01, 0.01, 0.01)
     model.position.set(-1, 0, 0)
-    scene.add(model, cube)
+    scene.add(model, cube, cube2)
 })
 
 
@@ -59,6 +66,8 @@ function animate() {
     controls.update()
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
+    cube2.rotation.x += 0.01
+    cube2.rotation.y += 0.01
 
     if (model) {
     model.rotation.y += 0.01
